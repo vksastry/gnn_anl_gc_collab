a50ce6965206159b0e9059b229e4b9e3e0c2eecd
Logan Ward <ward.logan.t@gmail.com>
2022-08-08 08:03:36 -0400

    Moved training data to a qm9 folder
    Make room for more datasets later

--- make_training_data.py
+++ make_training_data.py
@@ -1,3 +1,4 @@
+"""Download the QM9 dataset from GitHub, parse into train/test/validation sets"""
 import matplotlib
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -15,7 +16,7 @@ print(f'Set aside {len(test_set)} training entries')
 train_set, val_set = train_test_split(data.query('not in_holdout'), test_size=0.1, random_state=1)
 print(f'Split off {len(train_set)} training and {len(val_set)} validation entries')
 
-out_dir = Path('data')
+out_dir = Path('data') / 'qm9'
 out_dir.mkdir(exist_ok=True)
 
 for name, dataset in zip(['train', 'valid', 'test'], [train_set, val_set, test_set]):
